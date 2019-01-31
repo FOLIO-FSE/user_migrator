36e4ea6133ce047a50aa5b8f705e2496f5f11b85
Theodor Tolstoy <ttolstoy@ebsco.com>
2019-01-31 13:51:11 +0100

    improve error handling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import usaddress
 import itertools
 import csv
 import json
@@ -21,9 +23,16 @@ def chunks(myList, size):
      iterator = iter(myList)
      for first in iterator:
          yield itertools.chain([first], itertools.islice(iterator, size - 1))
-    # for i in range(0, len(list(myList)), n):
-    #    yield myList[i:i+n]
 
+
+def map_user_group(groups_map, user):
+    folio_group = next((g['Folio Code'] for g
+                 in groups_map
+                 if g['ILS code'] == user['patronGroup']), 'unmapped')
+    if folio_group == 'unmapped':
+        raise ValueError("source patron group error: {} for {}"
+                         .format(user['patronGroup'], user['id']))
+    return folio_group
 
 parser = argparse.ArgumentParser()
 parser.add_argument("source_path",
@@ -34,30 +43,55 @@ parser.add_argument("groups_map_path",
                     help="path of the group mapping file")
 parser.add_argument("mapper",
                     help="which mapper to use")
+parser.add_argument("source_name",
+                    help="source name")
 args = parser.parse_args()
 
 with open(args.groups_map_path, 'r') as groups_map_file:
     groups_map = list(csv.DictReader(groups_map_file))
     config = {"groupsmap": groups_map}
     mapper = get_mapper(args.mapper, config)
-    import_struct = {"source_type": "test_hampat",
+    import_struct = {"source_type": args.source_name,
                      "deactivateMissingUsers": False,
                      "users": [],
                      "updateOnlyPresentFields": False,
                      "totalRecords": 0}
     with open(args.source_path, 'r') as source_file:
+        file_name = os.path.basename(source_file.name).replace('.json','')
+        total_users = 0
         i = 0
-        users = mapper.get_users(source_file)
-        cs = chunks(users, 100)
-        for c in cs:
+        failed_users = 0
+        last_counter = dict()
+        users_json = mapper.get_users(source_file)
+        cs = chunks(users_json, 100)
+        for chunk in cs:
             i += 1
             import_struct["users"] = []
-            for user in c:
+            for user_json in chunk:
                 try:
-                    import_struct["users"].append(mapper.do_map(user))
+                    total_users += 1
+                    user = mapper.do_map(user_json[0])
+                    patron_group = map_user_group(groups_map, user)
+                    # patron group is mapped and set
+                    if patron_group != '':
+                        user['patronGroup'] = patron_group
+                        import_struct["users"].append(user)
+                    last_counter = user_json[1]
                     import_struct["totalRecords"] = len(import_struct["users"])
-                except Exception as ee:
-                    print(ee)
-            path = args.result_path + str(i) + '.json'
+                except ValueError as ee:
+                    failed_users += 1
+                    print(str(ee))
+                except usaddress.RepeatedLabelError as rle:
+                    failed_users += 1
+                    print("Failed parsing address for user")
+                    print(user_json)
+                    print(str(rle))
+            path = "{}{}_{}_{}.json".format(args.result_path,
+                                           args.source_name,
+                                           file_name,
+                                           str(i))
             with open(path, 'w+') as results_file:
                 results_file.write(json.dumps(import_struct, indent=4))
+        print("Number of failed users:\t{} out of {} in total"
+              .format(failed_users, total_users))
+        print(last_counter)
--- mappers/Chalmers.py
+++ mappers/Chalmers.py
@@ -1,3 +1,5 @@
+import dateutil.parser
+from datetime import datetime as dt
 import re
 import uuid
 import requests
@@ -21,33 +23,46 @@ class Chalmers:
 
     def do_map(self, user):
         return {"id": str(uuid.uuid4()),
-                "patronGroup": self.get_patron_group(user),
+                "patronGroup": str(user['patronType']),
                 "barcode": self.get_barcode(user),
                 "username": self.get_user_name(user),
                 "externalSystemId": self.get_ext_uid(user),
                 "active": self.get_active(user),
                 "personal": {"preferredContactTypeId": "mail",
-                             "lastName": '',
-                             "firstName": '',
-                             "phone": self.get_phone(user),
+                             "lastName": self.get_names(user)[0],
+                             "firstName": self.get_names(user)[1],
+                             "phone": '', # No phones!
                              "email": self.get_email(user),
                              "addresses": list(self.get_addresses(user))},
                 "expirationDate": user['expirationDate']}
 
     def get_users(self, source_file):
+        counters = { 'expired': 0,
+                     'suppressed': 0,
+                     'blocked': 0,
+                     'deleted': 0 }
         for line in source_file:
-            yield json.loads(line)
-
-    def get_patron_group(self, user):
-        # TODO: Map patronType to Patron Group
-        return user['patronType']
-
-    def get_phone(self, user):
-        return ''
+            user_json = json.loads(line)
+            if user_json['deleted'] == True:
+                counters['deleted'] += 1
+            elif user_json['suppressed'] == True:
+                counters['suppressed'] += 1
+            elif user_json['blockInfo']['code'] != '-':
+                counters['blocked'] += 1
+            else:
+                exp_date = dateutil.parser.parse(user_json['expirationDate']) 
+                if exp_date > dt.now():
+                    yield[user_json, counters]
+                else:
+                    counters['expired'] += 1
 
     def get_email(self, user):
-        if len(user['emails']) > 1:
+        if not 'emails' in user:
+            raise ValueError("No emails attribute for {}".format(user['id']))
+        elif len(user['emails']) > 1:
             raise ValueError("Too many emails for {}".format(user['id']))
+        elif len(user['emails']) == 0:
+            raise ValueError("Zero emails for {}".format(user['id']))
         else:
             eml = user['emails'][0]
             reg = r"[^@]+@[^@]+\.[^@]+"
@@ -58,48 +73,112 @@ class Chalmers:
                 return eml
 
     def get_barcode(self, user):
-        if len(user['barcodes']) > 1:
-            raise ValueError("Too many barcodes for {}".format(user['id']))
+        if 'barcodes' in user:
+            if len(user['barcodes']) > 1:
+                raise ValueError("Too many barcodes for {}"
+                                 .format(user['id']))
+            elif len(user['barcodes']) == 0:
+                raise ValueError("Zero barcodes for {}"
+                                 .format(user['id']))
+            else:
+                return user['barcodes'][0]
         else:
-            return user['barcodes'][0]
+            raise ValueError("No barcodes for  {}".format(user['id']))
 
-    def get_user_name(self, user):
-        if len(user['uniqueIds']) > 1:
-            raise ValueError("Too many unique ids for {}".format(user['id']))
+    def get_personnummer(self, user):
+        if 'uniqueIds' not in user:
+            raise ValueError("no uniqueIds attrib for {}"
+                             .format(user['id']))
+        elif len(user['uniqueIds']) > 1:
+            raise ValueError("Too many unique ids for {}"
+                             .format(user['id']))
+        elif len(user['uniqueIds']) == 0:
+            raise ValueError("Zero uniqueIds for {}"
+                             .format(user['id']))
         else:
             uid = user['uniqueIds'][0]
             if len(uid) == 10:
+                # print(uid)
                 return uid
             else:
-                raise ValueError("Correct personnummer ({}) for user {}?"
+                raise ValueError("Incorrect personnummer ({}) for user {}?"
                                  .format(uid, user['id']))
 
+    def get_user_name(self, user):
+        return self.get_personnummer(user)
+
     def get_ext_uid(self, user):
-        return ''
+        if user['patronType'] in [10, 11, 19, 20, 30]:
+            # User is chalmers affiliated and should use CID
+            email = self.get_email(user)
+            if 'chalmers.se' not in self.get_email(user):
+                raise ValueError("Not a propers CID ({}) for {}"
+                                 .format(email, user['id']))
+            return email
+        elif user['patronType'] in [110, 120, 130, 140, 150, 200, 201]:
+            # User is library and should use library code
+            print(user)
+        elif user['patronType'] in [30, 50, 60]:
+            # User is considered member of public. Personnummer.
+            return self.get_personnummer(user)
+        else:
+            raise ValueError("Unhandled patronType {} for {}"
+                             .format(user['patronType'], user['id']))
+
+
 
     def get_active(self, user):
-        return False
+        return True
 
     def get_names(self, user):
-        return ''
+        if 'names' not in user:
+            raise ValueError("no names attrib for {}".format(user['id']))
+        if len(user['names']) > 1:
+            raise ValueError("Too many names for {}".format(user['id']))
+        elif len(user['names']) == 0:
+            raise ValueError("Zero names for {}".format(user['id']))
+        elif ', ' not in user['names'][0]:
+            raise ValueError("No comma in name for {}".format(user['id']))
+        else:
+           return user['names'][0].split(', ')
 
     def get_expiration_date(self, user):
-        return ''
+        return user['expirationDate']
 
     def get_addresses(self, user):
         if 'addresses' not in user:
-            return []
-        for a in user['addresses']:
-            ls = a['lines']
-            yield {"countryId": '',
-                   "addressTypeId": '',
-                   "addressLine1": ls[0] if len(ls) > 0 else '',
-                   "addressLine2": ls[1] if len(ls) > 1 else '',
-                   "addressLine3": ' '.join(ls[2:]) if len(ls) > 2 else '',
-                   "region": '',
-                   "city": '',
-                   "primaryAddress": '',
-                   "postalCode": ''}
+            raise ValueError("no addresses attrib for {}".format(user['id']))
+        num_adr = len(user['addresses'])
+        if num_adr == 0:
+             raise ValueError("0 addresses for {}".format(user['id']))
+        elif num_adr > 2:
+             raise ValueError("Too many addresses for {}".format(user['id']))
+        else:
+            if num_adr == 1:
+                adr = self.parse_address(user['addresses'][0])
+                adr['addressTypeId'] = '2400fbeb-b369-45f3-8de0-362ce5d114a4'
+                adr['primaryAddress'] = True
+                return [adr]
+            elif num_adr == 2:
+                adr_inst = self.parse_address(user['addresses'][0])
+                adr_inst['addressTypeId'] = '6731169b-90e3-4dfe-8403-948c4432c3fc'
+                adr_inst['primaryAddress'] = True
+                adr_home = self.parse_address(user['addresses'][1])
+                adr_home['addressTypeId'] = '2400fbeb-b369-45f3-8de0-362ce5d114a4'
+                adr_home['primaryAddress'] = False
+                return [adr_home, adr_inst]
+
+    def parse_address(self, address):
+        ls = address['lines']
+        return {"countryId": '',
+                "addressTypeId": '',
+                "addressLine1": ls[0] if len(ls) > 0 else '',
+                "addressLine2": ls[1] if len(ls) > 1 else '',
+                "addressLine3": ' '.join(ls[2:]) if len(ls) > 2 else '',
+                "region": '',
+                "city": '',
+                "primaryAddress": '',
+                "postalCode": ''}
 
 
 def gen_dict_extract(key, var):
